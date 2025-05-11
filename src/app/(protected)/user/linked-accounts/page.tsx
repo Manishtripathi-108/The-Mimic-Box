2821554c566674f7574e044982316f174b4d8025
ishan <ishan.dhingra.work@gmail.com>
2025-05-11 12:31:32 +0530

    Refactor: Profile section

--- src/app/(protected)/user/linked-accounts/page.tsx
+++ src/app/(protected)/user/linked-accounts/page.tsx
@@ -27,15 +27,11 @@ const Page = async () => {
                             className="bg-primary border-primary shadow-floating-xs text-text-secondary hover:text-text-primary flex items-center justify-between rounded-lg border px-4 py-2 text-sm">
                             <div className="flex items-center gap-x-2">
                                 <Icon icon={account} className="mr-2 inline-block size-6" />
-                                <p className="font-medium capitalize">{account}</p>
+                                <h3 className="truncate overflow-hidden font-medium text-ellipsis whitespace-nowrap capitalize">{account}</h3>
                             </div>
 
                             {isLinked ? (
                                 <>
-                                    <span className="text-center font-medium text-green-600">
-                                        <Icon icon="link" className="mr-1 inline size-5" />
-                                        Connected
-                                    </span>
                                     <DisconnectAccount
                                         className="w-20 cursor-pointer text-center text-red-500 disabled:text-red-600"
                                         account={account}>
@@ -44,10 +40,6 @@ const Page = async () => {
                                 </>
                             ) : (
                                 <>
-                                    <span className="text-center font-medium text-red-600">
-                                        <Icon icon="unlink" className="mr-1 inline size-5" />
-                                        Not Connected
-                                    </span>
                                     <ConnectAccount
                                         callBackUrl={APP_ROUTES.USER_LINKED_ACCOUNTS}
                                         className="w-20 cursor-pointer text-center text-green-500 capitalize disabled:text-green-600"
