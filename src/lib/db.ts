dc0db69b08529ce96b9c975c2c8c96ec7587c351
Manishtripathi-108 <manish82644@gmail.com>
2025-08-22 18:10:56 +0530

    refactor: update Prisma configuration and email transport settings

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,11 @@
-import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+import { PrismaClient } from '../lib/generated/prisma';
 
-export const db = globalForPrisma.prisma || new PrismaClient();
+const globalForPrisma = global as unknown as {
+    prisma: PrismaClient;
+};
+
+export const db = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
